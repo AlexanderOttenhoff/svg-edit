a59153a776173b216e04f82c17326df1ef22faaa
Alexander Ottenhoff <alexander@sqreen.io>
2017-05-01 17:39:29 +0200

    App is now interactive svg

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import Logo from './logo';
-import './app.css';
 
-const App = () => (
-  <div className="App">
-    <div className="App-header">
-      <div className="App-logo" alt="logo">
-        <Logo />
-      </div>
-      <h2>Welcome to React</h2>
-    </div>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-);
+const SIZE = 20;
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 0,
+      y: 0,
+      clicked: false,
+    };
+  }
+
+  // componentDidUpdate() {
+  //   console.log(this.state);
+  // }
+
+  render() {
+    return (
+      <svg
+        onMouseMove={e => this.setState({ x: e.clientX, y: e.clientY })}
+        onMouseDown={() => this.setState({ clicked: true })}
+        onMouseUp={() => this.setState({ clicked: false })}
+      >
+        <rect fill={this.state.clicked ? 'green' : 'red'} width={SIZE} height={SIZE} x={this.state.x - (SIZE / 2)} y={this.state.y - (SIZE / 2)} />
+        {/* <text y={50} fontSize={50} stroke="black">Test</text> */}
+      </svg>
+    );
+  }
+}
 export default App;
